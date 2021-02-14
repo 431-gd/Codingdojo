f1fa7affb73e6142be04f78c958dd02799e65942
431-gd <78299822+431-gd@users.noreply.github.com>
2021-02-14 16:06:17 +0300

    Add files via upload

---
+++ week2/app.py
@@ -0,0 +1,98 @@
+from flask import Flask, jsonify, request
+# from flask_cors import CORS
+from blockchain import Blockchain
+from argparse import ArgumentParser
+import requests
+
+app = Flask(__name__)
+# CORS(app)
+
+
+@app.route('/', methods=['GET'])
+def chian():
+    chain = test.chain
+    dictChain = [block.__dict__.copy() for block in chain]
+    for dictBlock in dictChain:
+        dictBlock['transactions'] = [tx.__dict__ for tx in dictBlock['transactions']]
+    return jsonify(dictChain), 200
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    block = test.addBlock()
+    if block != None:
+        dicBlock = block._dict_.copy()
+        dicBlock['transactions'] = [tx._dict_ for tx in dictBlock['transactions']]
+    res = {
+        'Block' : dictBlock,
+    }
+    return jsonify(res), 201
+    else:
+        res = {
+            'Error': 'Not able to mine a block',
+        }
+        return jsonify(res), 500
+
+@app.route('/opentxs', methods=['GET'])
+def opentxs():
+    """ get the unconfirmed transactions or any transaction has not been included in a block """
+    
+    txs = test.unconfirmed
+    if txs != None:
+        dicTx = [tx._dict_ for tx in txs]
+        res = {
+            'Transactions': dicTx
+        }
+        return jsonify(res), 200
+        else:
+            res = {
+                'Message': 'There is no transaction'
+            }
+            return jsonify(res), 500
+
+@app.route('/sendtx', methods=['POST'])
+def sendtx():
+    """ send a transaction"""
+values = request.get_json()
+if not values:
+    res = {
+        'Massage': 'There is no input'
+    }
+    return jsonify(res), 400
+
+    reqKeys = ['sender', 'receiver', 'amount']
+
+    if not all (key in values in reqKeys):
+        res = {
+            'Message': 'There is a missing value'
+        }
+        return jsonify(res),400
+
+        sender = values['sender']
+        receiver = values['receiver']
+        amount = values['amount']
+        addTx = test.addTransaction(sender, receiver, amount)
+        if addTx !=None:
+            res = {
+                'Transaction': {
+                    'sender' : values['sender'],
+                    'receiver' : values['recevier'],
+                    'amount' : values ['amount']
+                }
+            }
+            return jsonify(res), 200
+            else :
+                res = {
+                    'Message' : 'The transaction does not pass'
+                }
+                return jsonify(res), 500
+         
+
+
+if __name__ == '__main__':
+    ser = ArgumentParser()
+    ser.add_argument('-p', '--port', default=8020)
+    args = ser.parse_args()
+    port = args.port
+    test = Blockchain()
+    app.run(debug=True, port=port)
